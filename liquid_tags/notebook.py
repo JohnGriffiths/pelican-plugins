ca958982db63ca4094e64aa80280fa9a4da9be6e
John Griffiths <j.davidgriffiths@gmail.com>
2015-06-17 01:55:07 +1000

    changed text again as doesn't allow non-ascii characters

--- liquid_tags/notebook.py
+++ liquid_tags/notebook.py
@@ -166,7 +166,7 @@ jQuery(document).ready(function($) {
     console.log($(this).children());
     $codearea.slideToggle(500, function () {
         $header.text(function () {
-            return $codearea.is(":visible") ? "(-‿-)" : "(ô‿ô)";
+            return $codearea.is(":visible") ? "(-‿-)" : "(o‿o)";
         });
     });
 });
